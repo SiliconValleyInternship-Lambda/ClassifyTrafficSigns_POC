4944ebe2d2f8c560d238dd08e90718a8ab148fd0
kimsoyeong <iloveit356@gmail.com>
2021-01-14 13:32:52 +0900

    initialize

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Slider from '@material-ui/lab/Slider'
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
